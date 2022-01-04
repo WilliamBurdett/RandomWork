0439cf89e435788bc97a8461879f356ddf0dd50f
WilliamBurdett <william.burdett1@gamil.com>
2022-01-04 17:01:02 -0500

    Updated to use more colors and change very 0.5 seconds

--- display_clock/application.py
+++ display_clock/application.py
@@ -1,30 +1,37 @@
 import tkinter
 from datetime import datetime
+from random import randint
 from time import sleep
 
 from utils.threading import Manager
 
 
 def draw_time(canvas: tkinter.Canvas):
-    black = "#fff"
-    white = "#000"
-    black_bg = True
+    colors = [
+        ("#ff0505", "#05ff05"),
+        ("#ff05de", "#deff05"),
+        ("#b005ff", "#ffda05"),
+        ("#2605ff", "#ff9305"),
+        ("#05fff7", "#ff4405"),
+        ("#000000", "#ffffff"),
+    ]
+    previous_color_index = -1
+    color_index = randint(0, len(colors) - 1)
     while True:
         canvas.delete("all")
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        if black_bg is True:
-            black_bg = False
-            canvas_color = white
-            text_color = black
-        else:
-            black_bg = True
-            canvas_color = black
-            text_color = white
+
+        while color_index == previous_color_index:
+            color_index = randint(0, len(colors) - 1)
+        base_color_index = randint(0, 1)
+        previous_color_index = color_index
+        canvas_color = colors[color_index][base_color_index]
+        text_color = colors[color_index][abs(base_color_index - 1)]
         canvas.configure(bg=canvas_color)
         canvas.create_text(
             5, 15, anchor=tkinter.W, font=("Purisa", 18), text=now, fill=text_color
         )
-        sleep(1)
+        sleep(0.5)
 
 
 def main():
